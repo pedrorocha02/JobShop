286c07c3fd9e99b21bda4046f7c39ddc17fee3b8
José Rocha <a26828@alunos.ipca.pt>
2023-05-22 19:22:29 +0100

    Read and write of files with timestamps

--- main.c
+++ main.c
@@ -1,88 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_JOBS 10
-#define MAX_MACHINES 10
-
-// https://github.com/ptal/kobe-scheduling/blob/master/data/jssp/ft/ft06.jss
-// https://ptal.github.io/scheduling-data.html#job-shop-scheduling-problem
-
-int time[MAX_JOBS][MAX_MACHINES];
-int sequence[MAX_JOBS][MAX_MACHINES];
-int schedule[MAX_JOBS][MAX_MACHINES];
-
-void calculateSchedule(int numJobs, int numMachines)
+typedef struct
 {
-    int machineTime[MAX_MACHINES] = {0};
+    int machineNumber;
+    int operationTime;
+} Item;
 
-    for (int i = 0; i < numJobs; i++)
-    {
-        for (int j = 0; j < numMachines; j++)
-        {
-            int machine = sequence[i][j];
-            int jobTime = time[i][machine];
-            int startTime = (machineTime[machine] > jobTime) ? machineTime[machine] : jobTime;
-
-            schedule[i][machine] = startTime;
-            machineTime[machine] = startTime + jobTime;
-        }
-    }
-}
-
-void printSchedule(int numJobs, int numMachines)
+void main()
 {
-    printf("Job Schedule:\n");
+    FILE *file = fopen("input.txt", "r");
 
-    for (int i = 0; i < numJobs; i++)
+    if (file == NULL)
     {
-        printf("Job %d:", i + 1);
-
-        for (int j = 0; j < numMachines; j++)
-        {
-            printf(" Machine %d (%d)", sequence[i][j] + 1, schedule[i][sequence[i][j]]);
-        }
-
-        printf("\n");
-    }
-}
-
-int main()
-{
-    int numJobs, numMachines;
-
-    printf("Enter the number of jobs: ");
-    scanf("%d", &numJobs);
-
-    printf("Enter the number of machines: ");
-    scanf("%d", &numMachines);
-
-    printf("Enter the processing times:\n");
-
-    for (int i = 0; i < numJobs; i++)
-    {
-        printf("Job %d:\n", i + 1);
-
-        for (int j = 0; j < numMachines; j++)
-        {
-            printf("Machine %d: ", j + 1);
-            scanf("%d", &time[i][j]);
-        }
+        printf("Failed to open the file.\n");
+        return;
     }
 
-    printf("Enter the job sequences:\n");
+    FILE *outputFile;
+    outputFile = fopen("output.txt", "w");
 
-    for (int i = 0; i < numJobs; i++)
+    if (outputFile == NULL)
     {
-        printf("Job %d: ", i + 1);
-
-        for (int j = 0; j < numMachines; j++)
-        {
-            scanf("%d", &sequence[i][j]);
-            sequence[i][j]--;
-        }
+        printf("Error opening the file.\n");
+        return;
     }
 
-    calculateSchedule(numJobs, numMachines);
-    printSchedule(numJobs, numMachines);
+    int numMachines, numJobs;
+    fscanf(file, "%d %d", &numMachines, &numJobs);
 
-    return 0;
+    Item items[numMachines * numJobs];
+
+    for (int i = 0; i < numMachines * numJobs; i++)
+    {
+        fscanf(file, "%d", &items[i].machineNumber);
+        fscanf(file, "%d", &items[i].operationTime);
+    }
+
+    fclose(file);
+
+    // Printing the read data
+    printf("Number of Machines: %d\n", numMachines);
+    printf("Number of Jobs: %d\n", numJobs);
+
+    int totalOperationTime = 0;
+    int startTime = 0;
+
+    for (int i = 0; i < numMachines * numJobs; i++)
+    {
+        fprintf(outputFile, "Job Number: %d, Operation Number: %d, Start Time: %d, Machine Number: %d, Operation Time: %d\n",
+                i / numMachines, i % numMachines, totalOperationTime, items[i].machineNumber, items[i].operationTime);
+        // Worst case scenario
+        printf("Machine Number: %d, Operation Time: %d\n", items[i].machineNumber, items[i].operationTime);
+        // Sum of the operation total (Similar to a fspan)
+        totalOperationTime += items[i].operationTime;
+    }
+
+    fclose(outputFile);
+    printf("Total Operation Time (including delays): %d\n", totalOperationTime);
+
+    return;
 }
--- test.c
+++ test.c
@@ -1,43 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct
+#define MAX_JOBS 10
+#define MAX_MACHINES 10
+
+// https://github.com/ptal/kobe-scheduling/blob/master/data/jssp/ft/ft06.jss
+// https://ptal.github.io/scheduling-data.html#job-shop-scheduling-problem
+
+int time[MAX_JOBS][MAX_MACHINES];
+int sequence[MAX_JOBS][MAX_MACHINES];
+int schedule[MAX_JOBS][MAX_MACHINES];
+
+void calculateSchedule(int numJobs, int numMachines)
 {
-    int machineNumber;
-    int operationTime;
-} Item;
+    int machineTime[MAX_MACHINES] = {0};
+
+    for (int i = 0; i < numJobs; i++)
+    {
+        for (int j = 0; j < numMachines; j++)
+        {
+            int machine = sequence[i][j];
+            int jobTime = time[i][machine];
+            int startTime = (machineTime[machine] > jobTime) ? machineTime[machine] : jobTime;
+
+            schedule[i][machine] = startTime;
+            machineTime[machine] = startTime + jobTime;
+        }
+    }
+}
+
+void printSchedule(int numJobs, int numMachines)
+{
+    printf("Job Schedule:\n");
+
+    for (int i = 0; i < numJobs; i++)
+    {
+        printf("Job %d:", i + 1);
+
+        for (int j = 0; j < numMachines; j++)
+        {
+            printf(" Machine %d (%d)", sequence[i][j] + 1, schedule[i][sequence[i][j]]);
+        }
+
+        printf("\n");
+    }
+}
 
 int main()
 {
-    FILE *file = fopen("input.txt", "r"); // Replace "input.txt" with the actual filename
+    int numJobs, numMachines;
 
-    if (file == NULL)
+    printf("Enter the number of jobs: ");
+    scanf("%d", &numJobs);
+
+    printf("Enter the number of machines: ");
+    scanf("%d", &numMachines);
+
+    printf("Enter the processing times:\n");
+
+    for (int i = 0; i < numJobs; i++)
     {
-        printf("Failed to open the file.\n");
-        return 1;
+        printf("Job %d:\n", i + 1);
+
+        for (int j = 0; j < numMachines; j++)
+        {
+            printf("Machine %d: ", j + 1);
+            scanf("%d", &time[i][j]);
+        }
     }
 
-    int numMachines, numJobs;
-    fscanf(file, "%d %d", &numMachines, &numJobs);
+    printf("Enter the job sequences:\n");
 
-    Item items[numMachines * numJobs];
-
-    for (int i = 0; i < numMachines * numJobs; i++)
+    for (int i = 0; i < numJobs; i++)
     {
-        fscanf(file, "%d", &items[i].machineNumber);
-        fscanf(file, "%d", &items[i].operationTime);
+        printf("Job %d: ", i + 1);
+
+        for (int j = 0; j < numMachines; j++)
+        {
+            scanf("%d", &sequence[i][j]);
+            sequence[i][j]--;
+        }
     }
 
-    fclose(file);
-
-    // Printing the read data
-    printf("Number of Machines: %d\n", numMachines);
-    printf("Number of Jobs: %d\n", numJobs);
-
-    for (int i = 0; i < numMachines * numJobs; i++)
-    {
-        printf("Machine Number: %d, Operation Time: %d\n", items[i].machineNumber, items[i].operationTime);
-    }
+    calculateSchedule(numJobs, numMachines);
+    printSchedule(numJobs, numMachines);
 
     return 0;
 }
